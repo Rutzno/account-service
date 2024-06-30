24c20605eb5946ac9cf7b2e7bb45cb2bf247bcff
Rutzno <d.rutzno@gmail.com>
2024-06-30 20:06:59 +0000

    add some requirements of security standard

---
+++ src/main/java/com/diarpy/accountservice/exception/PasswordException.java
@@ -0,0 +1,14 @@
+package accountservice.exception;
+
+/**
+ * @author Mack_TB
+ * @since 23/06/2024
+ * @version 1.0.3
+ */
+
+public class PasswordException extends RuntimeException {
+
+    public PasswordException(String message) {
+        super(message);
+    }
+}
