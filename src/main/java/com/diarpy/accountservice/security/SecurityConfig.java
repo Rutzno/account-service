4718a7017e100a1db66fe37ff387e481a49fb646
Rutzno <d.rutzno@gmail.com>
2024-06-30 18:38:08 +0000

    add authentication

---
+++ src/main/java/com/diarpy/accountservice/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package accountservice.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author Mack_TB
+ * @since 23/06/2024
+ * @version 1.0.2
+ */
+
+@Configuration
+public class SecurityConfig {
+
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    public SecurityConfig(RestAuthenticationEntryPoint restAuthenticationEntryPoint) {
+        this.restAuthenticationEntryPoint = restAuthenticationEntryPoint;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(restAuthenticationEntryPoint)) // Handle auth errors
+                .csrf(AbstractHttpConfigurer::disable) // For Postman
+                .headers(headers -> headers.frameOptions().disable()) // For the H2 console
+                .authorizeHttpRequests(auth -> auth  // manage access
+                        .requestMatchers(HttpMethod.POST, "/api/auth/signup").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll() // expose H2 console
+                        .requestMatchers("/actuator/shutdown").permitAll() // required for tests
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sessions -> sessions
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // no session
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
