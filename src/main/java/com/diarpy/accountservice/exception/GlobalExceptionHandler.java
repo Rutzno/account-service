3d5c5c25a50505e86aee3a3b2d605b01e40f809d
Rutzno <d.rutzno@gmail.com>
2024-06-30 21:26:46 +0000

    add authorization (ASVS V4)

--- src/main/java/com/diarpy/accountservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/diarpy/accountservice/exception/GlobalExceptionHandler.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 /**
  * @author Mack_TB
  * @since 23/06/2024
- * @version 1.0.4
+ * @version 1.0.5
  */
 
 @ControllerAdvice
@@ -21,22 +21,41 @@ public class GlobalExceptionHandler {
     public ResponseEntity<CustomErrorMessage> handleUserAlreadyExistsException(UserAlreadyExistException ex,
                                                                                WebRequest request) {
         CustomErrorMessage customErrorMessage = new CustomErrorMessage(LocalDateTime.now(),
-                400, "Bad Request", ex.getMessage(), request.getDescription(false).replace("uri=", ""));
+                400, "Bad Request", ex.getMessage(),
+                request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(customErrorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(PasswordException.class)
-    public ResponseEntity<CustomErrorMessage> handlePasswordException(PasswordException ex,
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<CustomErrorMessage> handleUserNotFoundException(UserNotFoundException ex,
+                                                                          WebRequest request) {
+        CustomErrorMessage customErrorMessage = new CustomErrorMessage(LocalDateTime.now(),
+                404, "Not Found", ex.getMessage(),
+                request.getDescription(false).replace("uri=", ""));
+        return new ResponseEntity<>(customErrorMessage, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<CustomErrorMessage> handleNotFoundException(NotFoundException ex,
                                                                       WebRequest request) {
+        CustomErrorMessage customErrorMessage = new CustomErrorMessage(LocalDateTime.now(),
+                404, "Not Found", ex.getMessage(),
+                request.getDescription(false).replace("uri=", ""));
+        return new ResponseEntity<>(customErrorMessage, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<CustomErrorMessage> handleBadRequestException(BadRequestException ex,
+                                                                        WebRequest request) {
         CustomErrorMessage customErrorMessage = new CustomErrorMessage(LocalDateTime.now(),
                 400, "Bad Request", ex.getMessage(),
                 request.getDescription(false).replace("uri=", ""));
         return new ResponseEntity<>(customErrorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(PaymentException.class)
-    public ResponseEntity<CustomErrorMessage> handlePaymentException(PaymentException ex,
-                                                                     WebRequest request) {
+    @ExceptionHandler(PasswordException.class)
+    public ResponseEntity<CustomErrorMessage> handlePasswordException(PasswordException ex,
+                                                                      WebRequest request) {
         CustomErrorMessage customErrorMessage = new CustomErrorMessage(LocalDateTime.now(),
                 400, "Bad Request", ex.getMessage(),
                 request.getDescription(false).replace("uri=", ""));
