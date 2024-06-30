5f6abd24bcdeffb023cd85ad7b64b7fab12fbe74
Rutzno <d.rutzno@gmail.com>
2024-06-30 20:11:51 +0000

    add business logic

--- src/main/java/com/diarpy/accountservice/entities/Payment.java
+++ src/main/java/com/diarpy/accountservice/entities/Payment.java
@@ -1,4 +1,55 @@
-package account.entities;
+package accountservice.entities;
 
+import jakarta.persistence.*;
+
+/**
+ * @author Mack_TB
+ * @since 23/06/2024
+ * @version 1.0.4
+ */
+
+@Entity
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String employee;
+    private String period;
+    private Long salary;
+
+
+    public Payment() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(String employee) {
+        this.employee = employee;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
 }
