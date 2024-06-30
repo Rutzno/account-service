b10727265b6600052fe831d588c54346ea4e789b
Rutzno <d.rutzno@gmail.com>
2024-06-30 21:41:58 +0000

    add authorization (ASVS V4)

---
+++ src/main/java/com/diarpy/accountservice/dtos/RoleDto.java
@@ -0,0 +1,44 @@
+package accountservice.dtos;
+
+import accountservice.enums.Operation;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+
+/**
+ * @author Mack_TB
+ * @since 23/06/2024
+ * @version 1.0.5
+ */
+
+public class RoleDto {
+    @NotBlank
+    private String user;
+    private String role;
+    @Enumerated(EnumType.STRING)
+    private Operation operation;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+}
