6b7b742c0b6e9072e31d0c80486fba59563054a6
Rutzno <d.rutzno@gmail.com>
2024-06-30 18:30:36 +0000

    add authentication

--- src/main/java/com/diarpy/accountservice/web/AuthController.java
+++ src/main/java/com/diarpy/accountservice/web/AuthController.java
@@ -1,7 +1,9 @@
 package account.web;
 
 import account.entities.MyUser;
+import account.service.MyUserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -10,26 +12,25 @@ import org.springframework.web.bind.annotation.*;
 /**
  * @author Mack_TB
  * @since 23/06/2024
- * @version 1.0.1
+ * @version 1.0.2
  */
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
+    private final MyUserService myUserService;
 
-    public AuthenticationController() {
-
+    @Autowired
+    public AuthController(MyUserService myUserService) {
+        this.myUserService = myUserService;
     }
 
     /**
      * allows the user to register on the service
      */
     @PostMapping("/signup")
-    public ResponseEntity<?> register(@RequestBody @Valid MyUser user, Errors errors) {
-        if (errors.hasErrors()) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(user);
+    public ResponseEntity<MyUser> register(@RequestBody @Valid MyUser user, Errors errors) {
+        return myUserService.register(user, errors);
     }
 
     /**
@@ -39,5 +40,10 @@ public class AuthController {
     @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
     public void changePassword(String oldPassword, String newPassword) {
 
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test AAA";
     }
 }
