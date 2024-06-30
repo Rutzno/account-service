46de6bcd75662025e280bb31d70125cada926789
Rutzno <d.rutzno@gmail.com>
2024-06-30 21:33:00 +0000

    add authorization (ASVS V4)

--- src/main/java/com/diarpy/accountservice/web/AdminController.java
+++ src/main/java/com/diarpy/accountservice/web/AdminController.java
@@ -1,42 +1,52 @@
 package accountservice.web;
 
-import org.springframework.http.HttpStatus;
+import accountservice.dtos.MyUserDto;
+import accountservice.dtos.RoleDto;
+import accountservice.service.AdminService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author Mack_TB
  * @since 23/06/2024
- * @version 1.0.1
+ * @version 1.0.5
  */
 
 @RestController
 @RequestMapping("api/admin")
 public class AdminController {
+    private final AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     /**
-     * changes user roles
+     * changes user roles (sets the roles)
      */
     @PutMapping("/user/role")
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public void updateRole() {
+    public ResponseEntity<MyUserDto> updateRole(@RequestBody @Valid RoleDto roleDto) {
+        return adminService.updateRole(roleDto);
+    }
 
+    /**
+     * obtains information about all users
+     */
+    @GetMapping( path = {"/user","/user/"})
+    public List<MyUserDto> getUsers() {
+        return adminService.findAllUsers();
     }
 
     /**
      * deletes a user
      */
-    @PutMapping("/user")
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public void deleteUser() {
-
-    }
-
-    /**
-     * displays information about all users
-     */
-    @GetMapping("/user")
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    public void getUsers() {
-
+    @DeleteMapping("/user/{email}")
+    public ResponseEntity<?> deleteUser(@PathVariable String email) {
+        return adminService.deleteUser(email);
     }
 }
